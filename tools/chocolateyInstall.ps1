b4842f2e3f6c101291841ba3b0c715042dca723b
Edward M. Meshuris <edward.meshuris@nordstrom.com>
2018-02-04 18:48:05 -0800

    init code commit

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,7 +2,19 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'jenkins'
-$zipFile = "jenkins.$ChocolateyPackageVersion.zip"
+$zipFile = "jenkins.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyUnzip -FileFullPath $zipFile -Destination $toolsDir -PackageName $packageName
+Expand-Archive .\payload\jenkins.zip -DestinationPath .
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $msiPath
+  softwareName  = 'jenkins*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
